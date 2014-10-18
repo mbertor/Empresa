88c452d880eb40c3c58c045019024f58041b186d
mbertor <phoenix.mbt@gmail.com>
2014-10-19 01:04:59 +0200

    new class

---
+++ Empresa.py
@@ -0,0 +1,8 @@
+__author__ = 'Raul'
+
+class Empresa():
+    def __init__(self,nombre_empresa,cif,razon_social):
+        self.nombre=nombre_empresa
+        self.cif=cif
+        self.razon_social=razon_social
+        self.lista_dpt=[]
